b3d89762385d881b45faac59f13361ee6b161da5
kobi <kobikis@gmail.com>
2015-03-16 12:15:46 +0200

    committed by mistake

--- sys-tests/src/test/java/com/gigaspaces/httpsession/qa/DeltaModeTestWithoutLogin.java
+++ sys-tests/src/test/java/com/gigaspaces/httpsession/qa/DeltaModeTestWithoutLogin.java
@@ -8,21 +8,21 @@ import com.gigaspaces.httpsession.sessions.DeltaStoreMode;
 
 public class DeltaModeTestWithoutLogin extends TestWithoutLoginBase {
 
-//	@Test
-//	public void testJboss() {
-//
-//		runJbossTest();
-//
-//		assertSpaceDeltaMode(USERS_VALUE, SystemTestCase.DEFAULT_SESSION_BASE_NAME);
-//	}
-//
-//	@Test
-//	public void testTomcat() {
-//
-//		runTomcatTest();
-//
-//		assertSpaceDeltaMode(USERS_VALUE, SystemTestCase.DEFAULT_SESSION_BASE_NAME);
-//	}
+	@Test
+	public void testJboss() {
+
+		runJbossTest();
+
+		assertSpaceDeltaMode(USERS_VALUE, SystemTestCase.DEFAULT_SESSION_BASE_NAME);
+	}
+
+	@Test
+	public void testTomcat() {
+
+		runTomcatTest();
+
+		assertSpaceDeltaMode(USERS_VALUE, SystemTestCase.DEFAULT_SESSION_BASE_NAME);
+	}
 
 	@Test
 	public void testJetty() {
