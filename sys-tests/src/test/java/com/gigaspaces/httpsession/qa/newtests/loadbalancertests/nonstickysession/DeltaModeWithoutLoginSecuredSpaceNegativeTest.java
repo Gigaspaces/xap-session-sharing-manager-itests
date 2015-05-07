669fcdabd6ff27bb565b5cacb553569d5643a010
yohana <yohaha@yohanakhoury.com>
2015-05-07 17:48:17 +0300

    GS-12341 Fixed SecuredSpaceNegative test

--- sys-tests/src/test/java/com/gigaspaces/httpsession/qa/newtests/loadbalancertests/nonstickysession/DeltaModeWithoutLoginSecuredSpaceNegativeTest.java
+++ sys-tests/src/test/java/com/gigaspaces/httpsession/qa/newtests/loadbalancertests/nonstickysession/DeltaModeWithoutLoginSecuredSpaceNegativeTest.java
@@ -1,6 +1,8 @@
 package com.gigaspaces.httpsession.qa.newtests.loadbalancertests.nonstickysession;
 
-import com.gigaspaces.httpsession.qa.newtests.bases.*;
+import com.gigaspaces.httpsession.qa.newtests.bases.AbstractLoadBalancerTest;
+import com.gigaspaces.httpsession.qa.newtests.bases.DeltaStoreModeBase;
+import com.gigaspaces.httpsession.qa.newtests.bases.WithoutLoginShiroSecurityConfiguration;
 import com.gigaspaces.httpsession.qa.utils.ExpectedTestResult;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,7 +22,7 @@ public class DeltaModeWithoutLoginSecuredSpaceNegativeTest extends AbstractLoadB
         super.expectedTestResult = new ExpectedTestResult(500, "");
         super.before();
         this.storeModeBase = new DeltaStoreModeBase();
-        this.shiroSecurityConfiguration = new WithoutLoginShiroSecurityConfigurationSecuredSpace();
+        this.shiroSecurityConfiguration = new WithoutLoginShiroSecurityConfiguration();
         this.webAppAddress = "http://localhost:7777/demo-app";
     }
 
