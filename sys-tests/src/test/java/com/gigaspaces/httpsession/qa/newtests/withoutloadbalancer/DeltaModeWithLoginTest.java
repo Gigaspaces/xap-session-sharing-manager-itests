94957e5e76bb4ac2f15e700ca30378a632d0da93
yohana <yohaha@yohanakhoury.com>
2015-06-03 09:49:25 +0300

    GS-12341 Changed port 7777 to 7778

--- sys-tests/src/test/java/com/gigaspaces/httpsession/qa/newtests/withoutloadbalancer/DeltaModeWithLoginTest.java
+++ sys-tests/src/test/java/com/gigaspaces/httpsession/qa/newtests/withoutloadbalancer/DeltaModeWithLoginTest.java
@@ -20,7 +20,7 @@ public class DeltaModeWithLoginTest extends AbstractTestBase {
         super.before();
         this.storeModeBase = new DeltaStoreModeBase();
         this.shiroSecurityConfiguration = new WithLoginShiroSecurityConfiguration();
-        this.webAppAddress = "http://localhost:7777/demo-app";
+        this.webAppAddress = "http://localhost:7778/demo-app";
     }
     @Test
     public void testJetty() throws IOException {
